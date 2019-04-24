43114e2f568a373feb6d26a3ab54c69019ecec71
Patiphan Phengpao <misterpatipan123@gmail.com>
2019-04-24 10:45:57 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YpfYu1Dc2TDnzHQ91zIN4bRYqGVafr2Bbh7R3SL7hra2CtPlrHmesgIvNzilJS8oUUiFmCgCuPbJG86G8G7Eob0cjxbywfMNrM9lL8rtqijSyM84nGQsHdFMSq9kiYq+VhO364veP3rgGMNCbP7HRgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
